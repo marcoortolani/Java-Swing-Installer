bada87eeebe5998e896859a5ed545c2824a3c67d
Sean Hellum <seanhellum45@gmail.com>
2019-10-08 23:08:23 +0000

    some stuff

---
+++ app.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class app {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Installer");
+        JButton button = new JButton("Install");
+        frame.add(button);
+        frame.setSize(400,400);
+    }
+}
---
+++ install.bash
@@ -0,0 +1,6 @@
+echo "Cloning repo"
+git clone https://github.com/JesterOrNot/pycalc.git
+cd pycalc
+echo "Installing pycalc"
+python3 setup.py install
+echo "Done"
