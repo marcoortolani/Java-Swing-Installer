e306ad7bf14bfe0ac11a1f8f111d816898e34279
Sean Hellum <seanhellum45@gmail.com>
2019-10-08 23:58:26 +0000

    changed stuff for pymath

--- install.bash
+++ install.bash
@@ -1,5 +1,5 @@
 echo "Cloning repo"
-git clone https://github.com/JesterOrNot/pycalc.git
+git clone https://github.com/JesterOrNot/pymath.git
 cd pycalc
 echo "Installing pycalc"
 python3 setup.py install
