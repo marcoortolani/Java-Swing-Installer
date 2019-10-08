19ace41757c04287d2d931fb93a7810ed323a1ce
Sean Hellum <seanhellum45@gmail.com>
2019-10-08 23:09:15 +0000

    more stuff for app.java

--- app.java
+++ app.java
@@ -4,6 +4,8 @@ public class app {
         JFrame frame = new JFrame("Installer");
         JButton button = new JButton("Install");
         frame.add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
+        frame.setVisible(true);
     }
 }
