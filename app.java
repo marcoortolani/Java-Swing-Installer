fdf09b1d2e1744aafd6af2c4307ce16acc60c5d5
Sean Hellum <seanhellum45@gmail.com>
2019-10-08 23:13:28 +0000

    add action listener

--- app.java
+++ app.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 public class app {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Installer");
         JButton button = new JButton("Install");
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ProcessBuilder pb = new ProcessBuilder("python3","graph.py");
+                try {
+                    Process p = pb.start();
+                } catch (IOException ee) {
+                    ee.printStackTrace();
+                }
+            }
+        });
         frame.add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
